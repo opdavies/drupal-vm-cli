79bcdd2f699451fa838829f06a8b1864588ae72a
Oliver Davies <oliver@oliverdavies.uk>
2016-02-02 09:51:33 +0000

    Remove PROJECT_ROOT

--- generate.php
+++ generate.php
@@ -5,7 +5,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use DrupalVmConfigGenerator\Console\Command\GenerateCommand;
 
-define('PROJECT_ROOT', __DIR__);
 $app = new Application();
 
 $app->add(new GenerateCommand());
