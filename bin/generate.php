25b4d20d9d6d85fc31e1158d1e5eaf710aec5018
Oliver Davies <oliver@oliverdavies.uk>
2016-02-10 18:05:18 +0000

    Add box.json to assist with phar creation
    Added box.json to allow for easy phar file creation using the
    Box project.
    
    See http://box-project.github.io/box2/

--- bin/generate.php
+++ bin/generate.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\Console\Application;
 use DrupalVmConfigGenerator\Console\Command\GenerateCommand;
 
-$app = new Application();
+$app = new Application('Drupal VM Config Generator', '@package_version@');
 
 $app->add(new GenerateCommand());
 
