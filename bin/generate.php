11edfca02c8679eb50c5927aace21ae81a9abc82
Oliver Davies <oliver@oliverdavies.uk>
2016-02-10 17:59:14 +0000

    Move logic into generate.php

---
+++ bin/generate.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use DrupalVmConfigGenerator\Console\Command\GenerateCommand;
+
+$app = new Application();
+
+$app->add(new GenerateCommand());
+
+$app->setDefaultCommand('generate');
+
+$app->run();
