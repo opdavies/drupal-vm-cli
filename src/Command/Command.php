14f3ab8ac76ff6a25a9a0830385d3135afe224d4
Oliver Davies <oliver@oliverdavies.uk>
2016-04-14 01:24:53 +0100

    Import Twig_Environment and Twig_Loader_Filesystem

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -7,6 +7,8 @@ use Symfony\Component\Console\Command\Command as BaseCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 abstract class Command extends BaseCommand
 {
@@ -26,7 +28,7 @@ abstract class Command extends BaseCommand
     protected $fs;
 
     /**
-     * @var \Twig_Environment
+     * @var Twig_Environment
      */
     protected $twig;
 
@@ -52,8 +54,9 @@ abstract class Command extends BaseCommand
 
         $this->fs = new Filesystem();
 
-        $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../../templates');
-        $this->twig = new \Twig_Environment($loader);
+        $this->twig = new Twig_Environment(
+            new Twig_Loader_Filesystem(__DIR__ . '/../../templates')
+        );
 
         $this->io = new DrupalVmStyle($input, $output);
     }
