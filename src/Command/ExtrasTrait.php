58a7ec8e5a3a9504f9247c2278ff5e70e66f3c62
Oliver Davies <oliver@oliverdavies.uk>
2016-05-14 21:12:00 +0100

    Fixed undefined offset error for installed_extras.
    Fixes #38

--- src/Command/ExtrasTrait.php
+++ src/Command/ExtrasTrait.php
@@ -6,6 +6,29 @@ use DrupalVmGenerator\Style\DrupalVmStyle;
 
 trait ExtrasTrait
 {
+    /**
+     * A list of available extra packages.
+     *
+     * @var array
+     */
+    private static $extras = [
+        'adminer',
+        'blackfire',
+        'drupalconsole',
+        'mailhog',
+        'memcached',
+        'newrelic',
+        'nodejs',
+        'pimpmylog',
+        'redis',
+        'ruby',
+        'selenium',
+        'solr',
+        'varnish',
+        'xdebug',
+        'xhprof',
+    ];
+
     public function extrasQuestion(DrupalVmStyle $io)
     {
         if ($io->confirm(
@@ -14,29 +37,15 @@ trait ExtrasTrait
         )
         ) {
             $selectedExtras = [];
+
             $io->writeln(
                 "\nType the package name from installed_extras or use keyup or keydown.\nThis is optional, press <info>enter</info> to <info>continue</info>.\n"
             );
 
-            $extras = [
-                'adminer',
-                'blackfire',
-                'drupalconsole',
-                'mailhog',
-                'memcached',
-                'newrelic',
-                'nodejs',
-                'pimpmylog',
-                'redis',
-                'ruby',
-                'selenium',
-                'solr',
-                'varnish',
-                'xdebug',
-                'xhprof',
-            ];
+            $extras = self::$extras;
 
             while (true) {
+                // Prompt the user to enter the name of the extra.
                 $extra = $io->choiceNoList(
                     'Enter the name of the extra',
                     $extras,
@@ -44,16 +53,22 @@ trait ExtrasTrait
                     true
                 );
 
+                // Remove any whitespace and ensure that the input is not empty.
                 $extra = trim($extra);
                 if (empty($extra)) {
                     break;
                 }
 
+                // Push the item into the selected extras array.
                 array_push($selectedExtras, $extra);
-                $key = array_search($extra, $extras, true);
 
+                // Remove the selected item from the available options.
+                $key = array_search($extra, $extras, true);
                 if ($key >= 0) {
                     unset($extras[$key]);
+
+                    // Reset the array keys to avoid undefined offset errors.
+                    $extras = array_values($extras);
                 }
             }
 
