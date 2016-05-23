8371b99871e2176c0f3767fd0f73b9597d5ea2da
Oliver Davies <oliver@oliverdavies.uk>
2016-05-23 17:21:29 +0100

    Make php_version configurable
    Fixes #42

--- src/Command/Config/GenerateCommand.php
+++ src/Command/Config/GenerateCommand.php
@@ -120,6 +120,12 @@ class GenerateCommand extends GeneratorCommand
                 'Install the site when the VM is provisioned'
             )
             ->addOption(
+                'php-version',
+                'null',
+                InputOption::VALUE_OPTIONAL,
+                'Which version of PHP to install'
+            )
+            ->addOption(
                 'installed-extras',
                 null,
                 InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY,
@@ -328,6 +334,18 @@ class GenerateCommand extends GeneratorCommand
             );
         }
 
+        // --php-version option
+        if (!$input->getOption('php-version')) {
+            $input->setOption(
+                'php-version',
+                $this->io->choice(
+                    'Which version of PHP',
+                    ['7.0', '5.6', '5.5'],
+                    '7.0'
+                )
+            );
+        }
+
         // --installed-extras option
         if (!$input->getOption('installed-extras')) {
             $input->setOption('installed-extras', $this->extrasQuestion($io));
@@ -394,6 +412,7 @@ class GenerateCommand extends GeneratorCommand
             'install_site' => $input->getOption('install-site'),
             'use_dashboard' => !$input->getOption('no-dashboard'),
             'keep_comments' => !$input->getOption('no-comments'),
+            'php_version' => $input->getOption('php-version'),
         ];
 
         $args['installed_extras'] = [];
