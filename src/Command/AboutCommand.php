9e340ec68d3592478296d237a93084bf9eda355d
Oliver Davies <oliver@oliverdavies.uk>
2017-01-19 00:02:43 +0000

    Update AboutCommand text

--- src/Command/AboutCommand.php
+++ src/Command/AboutCommand.php
@@ -34,30 +34,44 @@ class AboutCommand extends Command
             $application::SUPPORTED_DRUPAL_VM_VERSION
         ));
 
-        $commands = [
-            [
-                'Initialise Drupal VM CLI.',
-                'drupalvm init --overwrite',
-            ],
-            [
-                'Download the latest stable version of Drupal VM.',
-                'drupalvm new',
-            ],
-            [
-                'Generate a new configuration file.',
-                'drupalvm config:generate',
-            ],
-            [
-                'List all available commands.',
-                'drupalvm list',
-            ],
-        ];
+$content = <<<EOF
+This is the unofficial CLI app for <info>Drupal VM</info> to start new projects
+and generate configuration files.
 
-        foreach ($commands as $command) {
-            $io->comment($command[0]);
-            $io->newLine();
-            $io->text($command[1]);
-            $io->newLine();
-        }
+To download the latest version of Drupal VM, run the following command:
+
+  <comment>drupal-vm new</comment>
+
+This will automatically download Drupal VM and place it in a directory named
+<info>drupal-vm</info>.
+
+If you want to change the directory name, e.g. <info>myproject</info>, run the
+following the command changing the directory name as needed:
+
+  <comment>drupalvm new myproject</comment>
+
+To initialise Drupal VM CLI:
+
+  <comment>drupalvm init</comment>
+
+To generate a new <info>config.yml</info> file, run the following command:
+
+  <comment>drupalvm config:generate</comment>
+
+This command will fail if there is an existing config.yml file. To <info>overwrite
+an existing config.yml file</info>, run the following command:
+
+  <comment>drupalvm config:generate --overwrite</comment>
+
+Updating the Drupal VM CLI
+--------------------------
+
+The Drupal VM CLI is installed and managed by <info>Composer</info>. To update
+your Drupal VM CLI version, execute the following command:
+
+  <comment>composer global update opdavies/drupal-vm-cli</comment>
+
+EOF;
+        $output->writeln($content);
     }
 }
