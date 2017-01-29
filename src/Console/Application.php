6464f259975afce860c0ab9a6d88be4442d101e3
Oliver Davies <oliver@oliverdavies.uk>
2017-01-29 22:55:03 +0000

    Simplify imports

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -2,18 +2,12 @@
 
 namespace DrupalVm\Console;
 
-use DrupalVm\Command\AboutCommand;
-use DrupalVm\Command\Config\GenerateCommand as ConfigGenerateCommand;
-use DrupalVm\Command\InitCommand;
-use DrupalVm\Command\NewCommand;
 use Github\Client as GithubClient;
 use Github\HttpClient\CachedHttpClient;
 use GuzzleHttp\Client;
 use Opdavies\Twig\Extensions\TwigBooleanStringExtension;
 use Symfony\Component\Console\Application as ConsoleApplication;
 use Symfony\Component\Filesystem\Filesystem;
-use Twig_Environment;
-use Twig_Loader_Filesystem;
 
 class Application extends ConsoleApplication
 {
@@ -36,8 +30,8 @@ class Application extends ConsoleApplication
     {
         parent::__construct(self::NAME, self::VERSION);
 
-        $twig = new Twig_Environment(
-            new Twig_Loader_Filesystem(__DIR__.'/../../templates')
+        $twig = new \Twig_Environment(
+            new \Twig_Loader_Filesystem(__DIR__.'/../../templates')
         );
 
         $twig->addExtension(new TwigBooleanStringExtension());
@@ -51,10 +45,10 @@ class Application extends ConsoleApplication
         );
 
         $commands = [
-            new AboutCommand(),
-            new InitCommand($filesystem),
-            new NewCommand($client, $github),
-            new ConfigGenerateCommand($twig, $filesystem)
+            new \DrupalVm\Command\AboutCommand(),
+            new \DrupalVm\Command\InitCommand($filesystem),
+            new \DrupalVm\Command\NewCommand($client, $github),
+            new \DrupalVm\Command\Config\GenerateCommand($twig, $filesystem)
         ];
 
         $this->addCommands($commands);
